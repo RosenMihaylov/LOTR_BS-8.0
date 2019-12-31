fadd288ec2fede9ae82f9eed52a1e71300bb557d
RosenMihaylov <rosen_mihaylov@abv.bg>
2019-12-31 10:38:35 +0200

    connected to database, created users table

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+const { sequelize } = require("./models");
+const config = require("./config/config");
 
 const app = express();
 app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/register", (req, res) => {
-  res.send({
-    message: `You succesfuly registered as: ${req.body.userName}`
-  });
-});
+require("./routes")(app);
 
-app.listen(process.env.PORT || 8081);
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(config.port || 8081);
+    console.log(`Server started on Port ${config.port}`);
+  })
+  .catch(err => console.log(err));
---
+++ server/src/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || "LOTR-8.0",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "rosen",
+    options: {
+      dialect: process.env.DIALECT || "postgres",
+      host: process.env.HOST || "localhost"
+    }
+  }
+};
