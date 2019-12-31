d11906064483869abf59f7c808646eeecf7fcc6f
RosenMihaylov <rosen_mihaylov@abv.bg>
2019-12-31 23:29:52 +0200

    added material design icons

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,5 +1,41 @@
 <template>
   <div id="register">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Register form</v-toolbar-title>
+                <v-spacer />
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="email"
+                    name="email"
+                    prepend-icon="person"
+                    type="email"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
     <h1>This is a register page</h1>
     <br />
     <input
@@ -34,6 +70,9 @@ export default {
       error: ""
     };
   },
+  props: {
+    source: String
+  },
   methods: {
     async register() {
       if (!this.email || !this.password) {
@@ -66,6 +105,7 @@ export default {
 
 <style lang="scss" scoped>
 .error {
-  color: red;
+  background: #fff;
+  color: #fff;
 }
 </style>
