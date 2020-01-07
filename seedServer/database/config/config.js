e71dd6702fb8d10eea9d0a83ec4fd9b35f1628f6
RosenMihaylov <rosen_mihaylov@abv.bg>
2020-01-07 16:07:16 +0200

    changed the second server folder to seed folder

--- seedServer/database/config/config.js
+++ seedServer/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "postgres",
     password: "rosen",
-    database: "LOTR-database",
+    database: "LOTR-8.0",
     host: "127.0.0.1",
     dialect: "postgres"
   }
